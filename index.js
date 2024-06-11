6b193ed14a6dd854ff7b60005c052699338510dc
Nutan Shukla <20bcs147@iiitdmj.ac.in>
2024-06-11 15:35:10 +0530

    Changes the javascript file, I have wrote I own logic to calculate the answer instead of eval function

--- index.js
+++ index.js
@@ -5,11 +5,55 @@ const display = document.getElementById("display");
     display.value += char;
  }
   function calculate () {
-    try{
-    display.value = eval(display.value);
-    } catch(error) {
-        display.value = "Error in input";
-    }
+    try {
+      let expression = display.value;
+      let v = [];
+      let i = 0;
+
+      while (i < expression.length) {
+          if (expression[i] === '*' || expression[i] === '/') {
+              let x = parseInt(v.pop());
+              let y = parseInt(expression[i + 1]);
+              if (isNaN(x) || isNaN(y)) {
+                  throw "Invalid input";
+              }
+              let ans;
+              if (expression[i] === '*') {
+                  ans = x * y;
+              } else {
+                  ans = x / y;
+              }
+              v.push(ans.toString());
+              i += 2;
+          } else {
+              v.push(expression[i]);
+              i++;
+          }
+      }
+
+      let ans = parseInt(v[0]);
+      i = 1;
+      while (i < v.length) {
+          if (v[i] === '+' || v[i] === '-') {
+              let x = parseInt(v[i + 1]);
+              if (isNaN(x)) {
+                  throw "Invalid input";
+              }
+              if (v[i] === '+') {
+                  ans += x;
+              } else {
+                  ans -= x;
+              }
+              i += 2;
+          } else {
+              ans += parseInt(v[i]);
+              i++;
+          }
+      }
+      display.value = ans;
+  } catch (error) {x
+      display.value = "Error in input";
+    }
   }
    function clearDisplay() {
         display.value = "";
